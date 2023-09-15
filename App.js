dd1df677e192b380dfb7cdc21ed84f656350c6d3
hieprioku <130296530+hieprioku@users.noreply.github.com>
2023-09-15 22:03:12 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Shopping from "./components/Shopping";
+import "./App.css"
+import Cart from "./components/Cart";
+
+
+
+function App() {
+  return (
+    <div >
+      <Cart/>
+    </div>
+  );
+}
+
+export default App;
